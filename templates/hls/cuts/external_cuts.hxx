4ddb1c47136d2e30cdf6f9fca36290b9c2829f48
Bernhard Arnold <bernhard.arnold@cern.ch>
2018-09-27 16:32:56 +0200

    implemented external cut template

--- templates/hls/cuts/external_cuts.hxx
+++ templates/hls/cuts/external_cuts.hxx
@@ -1,9 +1,5 @@
 {% block code %}
-const {{ c.objects[0].type }}_obj_requ_t {{ c.name }}[{{ c.objects|count }}] = {
-{%- for o in c.objects %}
-    {
-        // tbd
-    },
-{% endfor -%}
+const {{ c.objects[0].type }}_obj_requ_t {{ c.name }} = {
+    .channel_id={{ c.objects[0].ext_channel_id }}
 };
 {% endblock code %}
