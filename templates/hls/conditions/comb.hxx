0367b67bdd6e2ec2664386b9f180603a23869df0
Bernhard Arnold <bernhard.arnold@cern.ch>
2018-09-28 16:49:16 +0200

    refactored objects

--- templates/hls/conditions/comb.hxx
+++ templates/hls/conditions/comb.hxx
@@ -1,3 +1,3 @@
 {% block code %}
-        {{ c.name }} = gtl::condition::{{ c.type }}<{{ c.objects[0].type }}_obj_requ_t, {{ c.objects[0].type }}_obj_t, {{ c.objects|count }}, {{ c.objects[0].slice.minimum }}, {{ c.objects[0].slice.maximum }}>(cuts::{{ c.name }}, in_data.{{ c.objects[0].type }});
+        {{ c.name }} = gtl::condition::{{ c.type }}<gtl::cut::{{ c.objects[0].type }}, gtl::object::{{ c.objects[0].type }}, {{ c.objects|count }}, {{ c.objects[0].slice.minimum }}, {{ c.objects[0].slice.maximum }}>(cuts::{{ c.name }}, in_data.{{ c.objects[0].type }});
 {%- endblock code %}
