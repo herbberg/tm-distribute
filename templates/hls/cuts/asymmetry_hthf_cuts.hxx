56f6b1aac288b8607c0b646b6c1ad10c2f4d19a9
herbberg <Herbert.Bergauer@oeaw.ac.at>
2018-10-05 10:36:49 +0200

    added more cuts

---
+++ templates/hls/cuts/asymmetry_hthf_cuts.hxx
@@ -0,0 +1,10 @@
+{% block code %}
+const gtl::cut::{{ c.objects[0].type }} {{ c.name }}[{{ c.objects|count }}] = {
+{%- for o in c.objects %}
+    {
+        .count={{ o.threshold|c_hex(2) }},
+        .comparison_mode={{ o.comparison_mode }},
+    },
+{%- endfor %}
+};
+{% endblock code %}
