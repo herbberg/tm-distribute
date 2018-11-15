afefaaa6c4e8f9910fcee3efcc918267e9c7ed08
herbberg <Herbert.Bergauer@oeaw.ac.at>
2018-11-15 15:26:06 +0100

    updated templates for bx

--- templates/hls/conditions/esums.hxx
+++ templates/hls/conditions/esums.hxx
@@ -1,3 +1,3 @@
 {% block code %}
-        {{ c.name }} = gtl::condition::esums(cuts::{{ c.name }}[0], in_data.{{ c.objects[0].type }}[0]);
+        {{ c.name }} = gtl::condition::esums(cuts::{{ c.name }}[0], in_data[bx_{{c.objects[0].bx}}].{{ c.objects[0].type }}[0]);
 {%- endblock code %}
--- templates/hls/conditions/signal.hxx
+++ templates/hls/conditions/signal.hxx
@@ -1,3 +1,3 @@
 {% block code %}
-        {{ c.name }} = gtl::condition::{{ c.type }}(cuts::{{ c.name }}, in_data.{{ c.objects[0].type }});
+        {{ c.name }} = gtl::condition::{{ c.type }}(cuts::{{ c.name }}, in_data[bx_{{c.objects[0].bx}}].{{ c.objects[0].type }});
 {%- endblock code %}
